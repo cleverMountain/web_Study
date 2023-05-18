c1d1d54e3f3c52d978df74ad35601cc41ad78ce6
Hjl <1031939925@qq.com>
2023-05-18 13:57:42 +0800

    @import与link区别

--- 浏览器/1.js
+++ 浏览器/1.js
@@ -1,2 +1,19 @@
-const d1 = document.getElementById('d1')
-console.log(d1, 1)
+// const d1 = document.getElementById('d1')
+// console.log(d1, 1)
+
+function init_module () {
+  initComputed()
+}
+
+
+const initComputed = (function () {
+ 
+  function init () {
+    console.log(1)
+  }
+  return function () {
+    init()
+  }
+})();
+
+
